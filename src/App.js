c712382121bf557a8bbcc5cbbbe8185a3817d887
Divit <hanma.oioioi@gmail.com>
2024-09-26 16:36:51 +0900

    add: completed todoApps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+    
+    // state変数の宣言
+    const [todos, setTodos] = useState([]);
+
+    const todoNameRef = useRef();
+
+    // タスクの追加
+    const handleAddTodo = () => {
+        const name = todoNameRef.current.value;
+        if (name === "") return;
+        setTodos((prevTodos) => {
+            return [...prevTodos, {id: uuidv4(), name: name, completed: false }];
+        });
+        todoNameRef.current.value = null;
+    };
+
+    // チェックボックスの切り替え
+    const toggleTodo = (id) => {
+        const newTodos = [...todos];
+        const todo = newTodos.find((todo) => todo.id === id);
+        todo.completed = !todo.completed;
+        setTodos(newTodos);
+    };
+
+    // 完了したタスクの削除
+    const handleClear = () => {
+        const newTodos = todos.filter((todo) => !todo.completed);
+        setTodos(newTodos);
+    };
+
     return (
         <div className="App">
-            <TodoList />
+            <TodoList todos={todos} toggleTodo={toggleTodo} />
+            <input type="text" ref={todoNameRef} />
+            <button onClick={handleAddTodo}>タスクを追加</button>
+            <button onClick={handleClear}>完了したタスクの削除</button>
+            <div>残りのタスク: {todos.filter((todo) => !todo.completed).length}</div>
         </div>
-        
     );
 }
 
